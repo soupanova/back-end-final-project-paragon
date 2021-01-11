3ee60c21d10f79a9c7e69686abf87c0c0ac6b155
crayola-eater <a-sheikh@outlook.com>
2021-01-11 23:39:35 +0000

    Changed playerGuessWhoseFact to align with question schema changes

--- controllers/factsGame/playGuessWhoseFact.js
+++ controllers/factsGame/playGuessWhoseFact.js
@@ -25,10 +25,8 @@ module.exports.playGuessWhoseFact = async ({
 }) => {
     {
         const game = await getGame({ gameId })
-        // const game = await initialiseTurn({ gameId })
-        const [question] = game.rounds[roundNumber - 1]
+        const { whoseFact: question } = game.rounds[roundNumber - 1]
         const participants = game.cachedChoices
-
         const leaderboard = createLeaderboard(Object.values(game.players))
 
         await broadcastForNSeconds({
@@ -38,7 +36,7 @@ module.exports.playGuessWhoseFact = async ({
                     gameId,
                     roundNumber,
                     action: actions.GUESS_WHO_TIMER,
-                    facts: question.statements,
+                    facts: question.facts,
                     participants,
                     leaderboard,
                     secondsLeft,
@@ -46,15 +44,6 @@ module.exports.playGuessWhoseFact = async ({
                 })
             },
         })
-
-        // Prompt clients for answers.
-        // broadcastToGame({
-        //     gameId,
-        //     action: actions.GUESS_WHO_CHOICE,
-        //     roundNumber,
-        //     turnId: game.currentTurnId,
-        //     participants,
-        // })
     }
 
     {
@@ -67,32 +56,26 @@ module.exports.playGuessWhoseFact = async ({
 
     {
         // Check + award players point where appropriate
-        // TODO: GAME INCREMENT_SCORES
-        // TODO: PLAYER INCREMENT_SCORE
-
-        // console.log('About to lock answers')
-        // const game = await lockAnswers({ gameId })
-        // console.log('Locked answers')
         const game = await getGame({ gameId })
-
-        const [question] = game.rounds[roundNumber - 1]
+        const { whoseFact: question } = game.rounds[roundNumber - 1]
 
         // Increment players' scores if they got the answer right.
         console.log("About to increment players' scores")
-        const playerIdsWhoAnsweredCorrectly = Object.values(game.players)
+
+        const allPlayers = Object.values(game.players)
+        const playerIdsWhichAnsweredCorrectly = allPlayers
             .filter(
-                ({ currentAnswer }) =>
-                    currentAnswer === question.correctAnswer.choiceId
+                (player) =>
+                    player.currentAnswer.choiceId === question.correctChoiceId
             )
             .map(({ playerId }) => playerId)
 
         await incrementPlayersScores({
             gameId,
-            playerIds: playerIdsWhoAnsweredCorrectly,
+            playerIds: playerIdsWhichAnsweredCorrectly,
         })
         console.log("Incremented players' scores")
 
-        // These things are unrelated and can be done at the same time.
         await broadcastForNSeconds({
             totalSeconds: secondsToWait.beforeReveal,
             broadcastFunc(secondsLeft) {
@@ -109,8 +92,7 @@ module.exports.playGuessWhoseFact = async ({
             gameId,
             action: actions.REVEAL_WHO,
             roundNumber,
-            displayName: question.correctAnswer.text,
-            // turnId: game.currentTurnId,
+            displayName: question.displayName,
         })
 
         await delay(secondsToWait.afterReveal)
